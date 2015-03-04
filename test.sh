fc13455fbb22e5230501bb29faad9c9ee3e5f33c
coutar-a <AGCoutarel@gmail.com>
2015-03-04 14:31:44 +0100

    maj script

--- test.sh
+++ test.sh
@@ -1,14 +1,27 @@
 #! /bin/bash
 
-declare -i x=0
-
 (sleep 30; killall lemipc)&
 
 xfce4-terminal -e ./lemipc
 
-for var in 0 1 2 3 4 5 6 7 8 9
+for var in {0..9}
 do
     xfce4-terminal -e ./lemipc
+done
+
+ME=`whoami`
+
+IPCS_S=`ipcs -s | egrep "0x[0-9a-f]+ [0-9]+" | grep $ME | cut -f2 -d" "`
+IPCS_M=`ipcs -m | egrep "0x[0-9a-f]+ [0-9]+" | grep $ME | cut -f2 -d" "`
+
+for id in $IPCS_S 
+do
+    ipcrm -s $id;
+done
+
+for id in $IPCS_M 
+do
+    ipcrm -m $id;
 done
 
 #./lemipc
